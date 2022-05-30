1c82b16823fcdcc15ae19f0dcc773a53b9a1dfe1
Prigorovskaya-j <prigorovskaya.j.i.2.18@gmail.com>
2022-05-30 23:22:52 +0300

    Закончен второй раздел по видео урокам.

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -10,8 +10,18 @@ export class CarsComponent{
 
   addCarStatus = false;
   carName = '';
+  cars = ['Ford', 'Audi', 'BMW', 'Mitsubishi', 'B', 'C', 'D'];
+  dates  = [
+    new Date(2015, 3, 4).toDateString(),
+    new Date(2014, 3, 4).toDateString(),
+    new Date(2015, 2, 1).toDateString(),
+    new Date(2017, 9, 9).toDateString()
+  ];
+ 
   addCar(){
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
   };
 
   constructor(){
